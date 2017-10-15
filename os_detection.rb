4ebfacee0368be79477ce74d1c5ac35cbd7c0512
Carlos Gomez <carlosgomez.deb@gmail.com>
2017-10-14 19:13:37 -0500

    os detection, ENV detection

---
+++ os_detection.rb
@@ -0,0 +1,17 @@
+# OS detection
+if RUBY_PLATFORM =~ /win32/
+  puts "We'rein Windows"
+elsif RUBY_PLATFORM =~ /linux/
+  puts "We're in Linux"
+elsif RUBY_PLATFORM =~ /darwin/
+  puts "We're in MacOS"
+elsif RUBY_PLATFORM =~ /freebsd/
+  puts "We're in FreeBSD"
+else
+  puts "We're running under an unknow OS"
+end
+
+print "\n\n"
+
+# ENV detection
+ENV.each { |e| puts e.join(': ') }
